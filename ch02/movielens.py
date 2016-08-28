4a130706178b09df38b013e26e37390113e0b16f
noprom <tyee.noprom@qq.com>
2016-08-29 06:21:46 +0800

    Order Series by value in descending order

--- ch02/movielens.py
+++ ch02/movielens.py
@@ -26,3 +26,22 @@ print movies[:5]
 data = pd.merge(pd.merge(ratings, users), movies)
 print data[:1]
 print data.ix[0]
+
+# To get mean movie ratings for each film grouped by gender, we can use the pivot_table method
+# Some bugs here
+# mean_ratings = data.pivot_table(ratings, rows='title', cols='gender', aggfunc='mean')
+# print mean_ratings[:5]
+
+ratings_by_title = data.groupby('title').size()
+print ratings_by_title[:10]
+
+# top 250 ratings
+active_titles = ratings_by_title.index[ratings_by_title >= 250]
+print active_titles
+
+# Standard deviation of rating grouped by title
+rating_std_by_title = data.groupby('title')['rating'].std()
+# Filter down to active_titles
+rating_std_by_title = rating_std_by_title.ix[active_titles]
+# Order Series by value in descending order
+print rating_std_by_title.sort_values(ascending=False)[:10]
