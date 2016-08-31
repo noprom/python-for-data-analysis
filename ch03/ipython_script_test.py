61797b23a596031cfc0f24aff1c4a740f4b200db
noprom <tyee.noprom@qq.com>
2016-08-31 08:04:48 +0800

    ipython bug & script test add

---
+++ ch03/ipython_script_test.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+'''
+@Title:ipython_script_test
+
+@Author: tyee.noprom@qq.com
+@Time: 8/29/16 5:03 PM
+'''
+
+
+def f(x, y, z):
+    return (x + y) / z
+
+
+a = 5
+b = 6
+c = 7.5
+result = f(a, b, c)
+print result
