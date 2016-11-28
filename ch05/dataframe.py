cb2331431132e692a9f7cb4c86ff063a80a7a80b
noprom <tyee.noprom@qq.com>
2016-11-28 11:01:05 +0800

    add ignore

--- ch05/dataframe.py
+++ ch05/dataframe.py
@@ -1,7 +1,40 @@
 # coding=utf-8
 '''
-@Title:
+@Title: DataFrame
 
 @Author: tyee.noprom@qq.com
 @Time: 9/2/16 6:42 PM
 '''
+
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+
+data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada'], 'year': [2000, 2001, 2002, 2001, 2002],
+        'pop': [1.5, 1.7, 3.6, 2.4, 2.9]}
+frame = DataFrame(data)
+frame = DataFrame(data, columns=['year', 'state', 'pop'])
+frame2 = DataFrame(data, columns=['year', 'state', 'pop', 'debt'], index=['one', 'two', 'three', 'four', 'five'])
+frame2.columns
+frame2['state']
+frame2.year
+frame2.ix['three']
+frame2['debt'] = 16.5
+frame2['debt'] = np.arange(5.)
+val = Series([-1.2, -1.5, -1.7], index=['two', 'four', 'five'])
+frame2['debt'] = val
+frame2['eastern'] = frame2.state == 'Ohio'
+del frame2['eastern']
+
+pop = {'Nevada': {2001: 2.4, 2002: 2.9}, 'Ohio': {2000: 1.5, 2001: 1.7, 2002: 3.6}}
+frame3 = DataFrame(pop)
+frame3.T
+DataFrame(pop, index=[2001, 2002, 2003])
+
+pdata = {'Ohio': frame3['Ohio'][:-1], 'Nevada': frame3['Nevada'][:2]}
+DataFrame(pdata)
+frame3.index.name = 'year'
+frame3.columns.name = 'state'
+frame3.values
+
+frame2.values
